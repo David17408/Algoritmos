86087b49666d40c2f17706184556562ac36dd796
David17408 <davidmartinezsc577@gmail.com>
2026-09-13 14:14:34 -0500

    Ejercicio 8.py
    Added comments to clarify triangle classification logic.

--- Ejercicio 8.py
+++ Ejercicio 8.py
@@ -7,18 +7,18 @@ while True:
     #por lados#
 
     if a == b == c:
-        print("El triángulo es equilátero")
+        print("El triángulo es equilátero") #Comprueba si los tres lados son iguales#
 
     elif a == b or b == c or a == c:
 
-        print("El triángulo es isóseles")
+        print("El triángulo es isóseles") #Comprueba si al menos dos lados son iguales#
 
     else:
 
-        print("El triángulo es escaleno")
+        print("El triángulo es escaleno") #En este caso todos los lados son distintos#
 
     #por ángulo#
-
+    #Busca el mayor de los lados y toma los restantes#
     mayor = max(a,b,c)
 
     if mayor == a:
@@ -27,7 +27,7 @@ while True:
         x,y = a,c
     else:
         x,y = a,b
-
+    #Compara el cuadrado del lado mayor con los cuadrados de los otros lados y clasifica el triángulo#
     if mayor**2 == x**2+y**2:
         print("Rectángulo")
     elif mayor**2 < x**2+y**2:
