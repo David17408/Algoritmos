5332d2d04f6a30d348aad2c27e69f438eb7c2b70
David17408 <davidmartinezsc577@gmail.com>
2026-09-13 14:08:42 -0500

    Ejercicio 7.py
    Refactor code for clarity and add comments explaining the logic.

--- Ejercicio 7.py
+++ Ejercicio 7.py
@@ -1,38 +1,38 @@
 n = int(input("Introduzca el número de datos: "))
 
-datos = []
+datos = [] #Crea una lista#
 
-for i in range(n):
+for i in range(n): #Repite el proceso#
 
     x = int(input("Introduzca un número: "))
-    datos.append (x)
+    datos.append (x) #Guarda los números introducidos#
 
 mayor = 0
 moda = 0
-
+#Recorre cada elemento de la lista#
 for x in datos:
 
     frecuencia = 0
-
+    #Compara x con los elementos de la lista#
     for y in datos:
 
-        if x == y:
+        if x == y: #Calcula la frecuencia comparandolo con cada dato y la aumenta en 1 cuando encuentra el mismo dato repetido#
 
             frecuencia = frecuencia + 1
-
+    #Si x aparece más veces que cualquier elemento anterior actualiza la mayor frecuencia encontrada#
     if frecuencia > mayor:
-
+    
         mayor = frecuencia
-        modas = [x]
+        modas = [x] #Crea una lista con la moda#
 
-    elif frecuencia == mayor:
+    elif frecuencia == mayor: #Si x tiene la misma frecuencia que la máxima encontrada la añade a la lista#
 
         modas.append(x)
 
-if mayor == 1:
+if mayor == 1: #Si todos los datos aparecen solo una vez#
 
     print ("No hay moda")
 
-else:
+else: #Cuando algun dato aparece más de una vez#
 
     print (f"La moda es:", modas)
