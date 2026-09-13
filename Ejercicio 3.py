88ef7d845c69a1383ae784fa9672592ceb9bd4f9
David17408 <davidmartinezsc577@gmail.com>
2026-09-13 13:12:30 -0500

    Ejercicio 3.py
    Added comments to clarify the purpose of each line.

--- Ejercicio 3.py
+++ Ejercicio 3.py
@@ -1,10 +1,10 @@
 frase = input("Introduzca una frase:")
 
-palabras = frase.split()
+palabras = frase.split() #Separa en palabras#
 
-inv_palabra = palabras[::-1]
+inv_palabra = palabras[::-1] #Invierte el orden de las palabras#
 
-inv_letras = frase[::-1]
+inv_letras = frase[::-1] #Invierte el orden de las letras#
 
 print(inv_letras)
 print(inv_palabra)
