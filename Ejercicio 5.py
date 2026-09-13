f970efaad99c833c9615231d22f2a2ba3ecedd84
David17408 <davidmartinezsc577@gmail.com>
2026-09-13 13:36:32 -0500

    Ejercicio 5.py
    Added comments to clarify the purpose of each section.

--- Ejercicio 5.py
+++ Ejercicio 5.py
@@ -1,19 +1,15 @@
-A = []
+A = [] #Se crea la lista#
 
 num = int(input("Número de elementos: "))
 
-for _ in range(num):
+for _ in range(num): #Se introducen los elementos#
         A.append(int(input("elemento?: ")))
 
-pares = [x for x in A if x % 2 == 0]
+pares = [x for x in A if x % 2 == 0] #Lista con los pares#
 
 print(A)
 print(pares)
 
-prompar = sum(pares) / len(pares)
+prompar = sum(pares) / len(pares) #Promedia los pares#
 
-if num != 0:
-    print(f"El promedio de los pares es: {prompar}")
-
-else:
-    print("No hay pares")
+print(f"El promedio de pares es: ", prompar)
