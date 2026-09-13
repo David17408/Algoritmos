fdae542a32494662432ed4664f4b0e70a79adde6
David17408 <davidmartinezsc577@gmail.com>
2026-09-13 14:30:49 -0500

    Ejercicio 10.py
    Added print statement for the largest prime found.

--- Ejercicio 10.py
+++ Ejercicio 10.py
@@ -3,25 +3,25 @@ n = int(input("Introduzca un número: "))
 cantidad = 0
 mayor = 0
 
-for num in range(2, n + 1):
+for num in range(2, n + 1):  #Recorre los números desde 2 hasta n##
 
     primo = True
 
-    divisor = 2
+    divisor = 2 
 
-    while divisor**2 <= num:
-
-        if num % divisor == 0:
-
+    while divisor**2 <= num: #Prueba divisores hasta la raiz cuadrada de num#
+    
+        #Comprueba si num es divisible entre divisor#
+        if num % divisor == 0: 
             primo = False
 
         divisor = divisor + 1
-
+    #Si no encuentra divisores el número es primo#
     if primo:
 
         print(num)
         cantidad = cantidad + 1
-        mayor = num
+        mayor = num #Actualiza al mayor primo encontrado#
 
 print("Cantidad de primos:", cantidad)
 print("Mayor primo:", mayor)
