9ab1becef81f899b8579a08f4654a883010f2229
David17408 <davidmartinezsc577@gmail.com>
2026-09-13 13:17:45 -0500

    Ejercicio 4.py
    Refactor code for clarity and add comments explaining logic.

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -3,12 +3,13 @@ n = int(input("Introduzca un número"))
 resultado = 0
 pos = 1
 
-while n > 0:
+while n > 0: #Repite mientras queden cifras por procesae
 
-    cifra = n % 10
-    resultado += cifra * pos
-    n //= 10
+    cifra = n % 10 #Obtiene la última cifra#
+    resultado += cifra * pos #Añade la cifra multiplicada por su posición#
+    n //= 10 #Elimina la última cifra#
 
+    #Si quedan cifras aumenta la posición dos lugares#
     if n > 0:
 
         pos *= 100
