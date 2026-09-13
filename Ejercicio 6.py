95510cc1eec8c3e75b5234636dde12b70cb958ba
David17408 <davidmartinezsc577@gmail.com>
2026-09-13 13:54:59 -0500

    Ejercicio 6.py
    Added comments to clarify the purpose of each section.

--- Ejercicio 6.py
+++ Ejercicio 6.py
@@ -1,20 +1,21 @@
 n = int(input("Introduzca el número de datos: "))
 
-datos = []
+datos = [] #Crea una lista#
 
-for i in range (n):
+for i in range (n): #Repite n veces para ingresar todos los datos#
 
-    x = float(input("Ingrese los datos:"))
+    x = float(input("Ingrese los datos:")) #Agrega el dato a la lista#
     datos.append(x)
 
-prom = sum(datos)/n
+prom = sum(datos)/n #Calcula el promedio de los datos#
 
 suma = 0
 
-for x in (datos):
+for x in (datos): #Recorre todos los datos de la lista#
+    
+    suma = suma + (x - prom)**2 #Hace el numerador de las formulas de desviación#
 
-    suma = suma + (x - prom)**2
-
+#Calcula las desviaciones#
 desv = (suma / n)**(0.5)
 desv2 = (suma / (n-1))**(0.5)
 
