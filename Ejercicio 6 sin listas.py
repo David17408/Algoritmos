f144a165e41ecb6d63523fcef2b7b33d9817a0ca
David17408 <davidmartinezsc577@gmail.com>
2026-09-13 13:42:18 -0500

    Ejercicio 6 sin listas.py
    Added comments to clarify the code's functionality.

--- Ejercicio 6 sin listas.py
+++ Ejercicio 6 sin listas.py
@@ -5,15 +5,18 @@ n = int(input("Introduzca el número de datos: "))
 suma = 0
 suma2 = 0
 
+#Repite el proceso n veces#
 for i in range (n):
 
     x = float(input("Introduzca el dato: "))
-
+    #Se acumula la suma de los datos y de sus cuadrados#
     suma = suma + x
     suma2 = suma2 + x**2
 
+    #Calcula el promedio#
     prom = suma / n
 
+#Calcula las desviaciones estandar#
 desv = ((suma2 - n*prom**2)/n)**0.5
 desv2 = ((suma2 - n*prom**2)/(n-1))**0.5
 
