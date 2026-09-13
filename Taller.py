9522c11371c3a0f4e959ff54a3645425e0a11864
David17408 <davidmartinezsc577@gmail.com>
2026-09-13 14:39:57 -0500

    Ejercicio 1.py
    Added comments for clarity and fixed incorrect variable usage in cosine calculation.

--- Taller.py
+++ Taller.py
@@ -2,7 +2,7 @@ import math
 
 opcion = 0
 
-while opcion != 4:
+while opcion != 4: #Repite el menú hasta que elija salir#
     print("Introduzca el número de la opción")
     print("1. Seno")
     print("2. Coseno")
@@ -10,9 +10,10 @@ while opcion != 4:
     print("4. Salir")
     opcion = int(input("Ingrese la opcion:"))
     
-    if (opcion == 1):
+    if (opcion == 1): 
         print("Esta cosa calcula la serie de Taylor del seno =)")
-        
+
+        #Convierte los grados a radianes#
         grados = float(input("Introduzca el valor en grados:"))
         
         n = int(input("Introduzca el numero de terminos"))
@@ -20,7 +21,7 @@ while opcion != 4:
         x = (grados * math.pi)/180
         
         seno = 0
-        
+        #Calcula el seno usando la serie de Taylor#
         for k in range (n):
         
             seno +=(((-1)**k)*x**(2*k+1))/math.factorial(2*k+1)
@@ -31,6 +32,7 @@ while opcion != 4:
         
         print("Esta cosa calcula la serie de Taylor del coseno =)")
         
+         #Convierte los grados a radianes#
         grados = float(input("Introduzca el valor en grados:"))
         
         n = int(input("Introduzca el numero de terminos"))
@@ -38,7 +40,8 @@ while opcion != 4:
         x = (grados * math.pi)/180
         
         coseno = 0
-        
+
+        #Calcula el coseno usando la serie de Taylor#
         for k in range (n):
         
             coseno += (((-1)**n)*(x**(2*n)))/math.factorial(2*n)
@@ -47,8 +50,8 @@ while opcion != 4:
 
     elif (opcion == 3):
         print("No se hace :3c")
-    elif (opcion == 4):
+    elif (opcion == 4): #Finaliza el proceso#
         print("cualquier cosa")
 
-else:
+else: #En caso de seleccionar una opción que no este, indicará que la opción es incorrecta#
     print("opcion incorrecta")
