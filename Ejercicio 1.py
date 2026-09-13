cfd7b5ce22ac1e5902d89fef41263e6d50aae4be
David17408 <davidmartinezsc577@gmail.com>
2026-09-13 13:00:22 -0500

    Ejercicio 1.py
    Added comments to clarify the calculation of delta_x and the integral process.

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -3,9 +3,11 @@ a=float(input("Límite inferior"))
 b=float(input("Límite superior"))
 n=int(input("Número de subintervalos"))
 
+#Calcula el ancho de cada subintervalo#
 delta_x = (b-a)/n
 suma = 0
 
+#Calcula la integral recorriendo cada subbintervalo#
 for i in range(n):
     x = (a+i*delta_x)
     y = eval(f)
