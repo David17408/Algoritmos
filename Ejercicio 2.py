03ede18434912eb67c3cda46039d18194b793fa1
David17408 <davidmartinezsc577@gmail.com>
2026-09-13 13:03:06 -0500

    Ejercicio 2.py
    Refactor input and comments for clarity.

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -1,10 +1,10 @@
 n = int(input("introduzca un número"))
 b = int(input("introduzca el número de la base"))
 
-k = ""
+k = "" #lista que guarda los dígitos#
 
 while n > 0:
-    k = str(n % b) + k
+    k = str(n % b) + k #Obtiene el residuo y lo coloca al inicio porque los dígitos se obtienen de derecha a izquierda
     n = n // b
 
 print(k)
