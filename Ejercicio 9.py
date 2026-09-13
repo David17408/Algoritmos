c90ff343fd67455696e4483c25db103bfbba1a29
David17408 <davidmartinezsc577@gmail.com>
2026-09-13 14:17:50 -0500

    Ejercicio 9.py
    Added comments for clarity and improved readability.

--- Ejercicio 9.py
+++ Ejercicio 9.py
@@ -6,15 +6,15 @@ suma = 0
 n = 0
 termino = 1
 
-while abs(termino) >= t:
-
-    termino = ((-1)**n)/(2*n + 1)
+while abs(termino) >= t: #Continúa mientras el término sea mayor a la tolerancia#
+#Usa la serie de leibniz para la aproximación#
+    termino = ((-1)**n)/(2*n + 1) 
 
     suma += termino
     n += 1
 
 aprox = suma*4
-error = abs(math.pi - aprox)
+error = abs(math.pi - aprox) #Calcula el error#
 
 print(f"El valor aproximado de pi es: ", aprox)
 print(f"El error es: ", error)
